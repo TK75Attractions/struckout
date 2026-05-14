fe142757f54b5e9dd5b7cc3ea6ee3575559e8848
taichi765 <taichi0209.youtub@gmail.com>
2026-05-14 17:09:28 +0900

    feat: Use devcontainer for struckout-projector

--- projector/src/main.rs
+++ projector/src/main.rs
@@ -1,9 +1,14 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::error::Error;
+use std::{error::Error, sync::mpsc};
 
 slint::include_modules!();
+
+struct Target{
+    point: i32,
+    color: slint::Color
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
@@ -14,6 +19,12 @@ fn main() -> Result<(), Box<dyn Error>> {
             let ui = ui_handle.unwrap();
             ui.set_counter(ui.get_counter() + 1);
         }
+    });
+
+    let (tx,rx)=mpsc::channel();
+
+    std::thread::spawn(move||{
+
     });
 
     ui.run()?;
