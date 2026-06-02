d47004b96ab739aa62518b8e651fd5f23d1997e9
taichi765 <taichi0209.youtub@gmail.com>
2026-06-02 12:11:37 +0900

    feat: Generate kotlin code from protobuf

--- ball_watcher/build.rs
+++ ball_watcher/build.rs
@@ -1,4 +1,4 @@
 fn main() -> std::io::Result<()> {
-    prost_build::compile_protos(&["protocol/struckout.proto"], &["protocol/"])?;
+    prost_build::compile_protos(&["../protocol/struckout.proto"], &["../protocol/"])?;
     Ok(())
 }
