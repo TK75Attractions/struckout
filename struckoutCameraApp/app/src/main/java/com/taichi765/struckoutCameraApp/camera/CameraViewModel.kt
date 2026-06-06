e3840a946436cee3b592bed4dcfcd261bba2bfd8
taichi765 <taichi0209.youtub@gmail.com>
2026-06-06 23:05:45 +0900

    refactor: Separate WorldDirectionCalculator to its own file

--- struckoutCameraApp/app/src/main/java/com/taichi765/struckoutCameraApp/camera/CameraViewModel.kt
+++ struckoutCameraApp/app/src/main/java/com/taichi765/struckoutCameraApp/camera/CameraViewModel.kt
@@ -5,16 +5,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.taichi765.struckoutCameraApp.camera.types.FrameID
-import com.taichi765.struckoutCameraApp.camera.types.WorldDirection
 import com.taichi765.struckoutCameraApp.transport.UdpTransportRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import org.opencv.calib3d.Calib3d
-import org.opencv.core.Core
-import org.opencv.core.CvType
-import org.opencv.core.Mat
-import org.opencv.core.Rect
 import struckout.v1.detectedObject
 import struckout.v1.udpPacket
 
@@ -61,54 +55,3 @@ class CameraViewModel(
         const val TAG = "CameraViewModel"
     }
 }
-
-/**
- * @constructor cameraRotationVector is the vector of `(theta*ax, theta*ay, theta*az)`.
- *
- * See [Android Document](https://developer.android.com/reference/android/hardware/camera2/CameraCharacteristics#LENS_POSE_ROTATION) for more information.
- *
- */
-class WorldDirectionCalculator(val cameraMatrix: Mat, cameraRotationVector: Mat) {
-    init {
-        require(cameraMatrix.height() == 3 && cameraMatrix.width() == 3) { "size of cameraMatrix was incorrect: expected 3x3" }
-        require(cameraRotationVector.height() == 3 && cameraRotationVector.width() == 1) { "size of cameraRotationVector was incorrect: expected 3x1" }
-    }
-
-    // convert rotation vector to rotation matrix.
-    private val cameraRotationMatrix = run {
-        val dst = Mat()
-        Calib3d.Rodrigues(cameraRotationVector, dst)
-        dst
-    }
-
-
-    fun calc(rect: Rect): WorldDirection {
-        // カメラ座標系でのピクセル値 (u,v,1)
-        val cameraCoordinate = Mat(3, 1, CvType.CV_64F).apply {
-            put(0, 0, (rect.x + rect.width / 2).toDouble())
-            put(1, 0, (rect.y + rect.height / 2).toDouble())
-            put(2, 0, 1.0)
-        }
-
-        // 正規化カメラ座標
-        val normalizedCameraCoordinate = run {
-            val dst = Mat()
-            Core.gemm(cameraMatrix.inv(), cameraCoordinate, 1.0, Mat(), 0.0, dst)
-            dst
-        }
-
-        val worldDirection = run {
-            val dst = Mat()
-            Core.gemm(cameraRotationMatrix, normalizedCameraCoordinate, 1.0, Mat(), 0.0, dst)
-            dst
-        }
-
-        return WorldDirection(
-            x = worldDirection.get(0, 0).first().toFloat(),
-            y = worldDirection.get(1, 0).first().toFloat(),
-            z = worldDirection.get(2, 0).first().toFloat()
-        )
-    }
-}
-
-
