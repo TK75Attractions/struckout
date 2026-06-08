8f85ace03620b788c8ba8a83db76fb3b5cb21f4a
ME <75.oda.yuto@tsukukoma-gafe.org>
2026-06-08 22:12:06 +0900

    feat: Add Test Listner for Rust

--- testTCP/src/main.rs
+++ testTCP/src/main.rs
@@ -1,3 +1,22 @@
+use std::net::TcpListener;
+use std::io::{Read, Write};
+
 fn main() {
     println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:5000")
+        .expect("Failed to open the Listener");
+    println!("Successfully opened the Listener"); 
+
+    for stream in listener.incoming()
+    {
+        match stream {
+        Ok(stream) => {
+            stream.write_all("Hello\n".as_bytes()).expect("Failed to write to stream");
+            stream.flush().expect("Failed to flush stream");
+            println!("New connection established");
+        }
+        Err(e) => {
+            println!("Failed to establish a connection: {}", e);
+        }
+    }
 }
