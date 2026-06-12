32e907ce035a2311145ad19918d2ef0a993181bd
taichi765 <taichi0209.youtub@gmail.com>
2026-06-12 11:14:27 +0900

    feat(camera): Add compile flag to switch NATS logging

---
+++ struckoutCameraApp/app/src/main/java/com/taichi765/struckoutCameraApp/NatsLoggingTree.kt
@@ -0,0 +1,9 @@
+package com.taichi765.struckoutCameraApp
+
+import timber.log.Timber
+
+class NatsLoggingTree : Timber.Tree() {
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        TODO("Not yet implemented")
+    }
+}
