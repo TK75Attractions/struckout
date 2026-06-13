ea6aa0046001b460ca26c2741d27edf742e76771
ME <75.oda.yuto@tsukukoma-gafe.org>
2026-06-13 22:15:21 +0900

    feat(Unity): Add Target Definition Files

---
+++ projector_Unity/Assets/Scripts/Domain/TargetDefinition/Target.cs
@@ -0,0 +1,21 @@
+namespace Struckout.Domain
+{
+    public class Target
+    {
+        public TargetCoordinate Coordinate { get; private set; }
+        public TargetType Type { get; private set; }
+        public float Size { get; private set; }
+
+        public Target(
+            TargetCoordinate coordinate,
+            TargetType type,
+            float size
+        )
+        {
+            Coordinate = coordinate;
+            Type = type;
+            Size = size;
+        }
+
+    }
+}
---
+++ projector_Unity/Assets/Scripts/Domain/TargetDefinition/TargetCoordinate.cs
@@ -0,0 +1,8 @@
+namespace Struckout.Domain
+{
+    public struct TargetCoordinate
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+}
