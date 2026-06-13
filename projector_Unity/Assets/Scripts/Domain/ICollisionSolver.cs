ce34091de76dfda1e392374bb560b354dcea5165
ME <75.oda.yuto@tsukukoma-gafe.org>
2026-06-13 22:45:12 +0900

    chore: Add Interface to CollsionSolver

---
+++ projector_Unity/Assets/Scripts/Domain/ICollisionSolver.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Struckout.Dto.V1;
+
+namespace Struckout.Domain
+{
+    public interface ICollisionSolver
+    {
+        bool IsCollision(CollisionPoint collisionPoint, List<Target> targets, out int targetPoint);
+    }
+}
