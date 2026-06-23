f1916ad9cc0409ae44bca31aa6f73c7a257bf7de
taichi765 <taichi0209.youtub@gmail.com>
2026-06-24 00:37:31 +0900

    chore(deps): Add hilt to dependenciess

--- struckoutCameraApp/build.gradle.kts
+++ struckoutCameraApp/build.gradle.kts
@@ -1,3 +1,4 @@
 // Top-level build file where you can add configuration options common to all subprojects/modules.
 plugins {
+    id("com.google.dagger.hilt.android") version "2.59.2" apply false
 }
