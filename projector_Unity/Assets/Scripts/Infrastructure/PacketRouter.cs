c385f6f699d91681dd3a5da0c46ce51835ecdab1
ME <75.oda.yuto@tsukukoma-gafe.org>
2026-06-23 11:42:02 +0900

    chore: Use RealImpl

--- projector_Unity/Assets/Scripts/Infrastructure/PacketRouter.cs
+++ projector_Unity/Assets/Scripts/Infrastructure/PacketRouter.cs
@@ -29,12 +29,14 @@ namespace Struckout.Infrastructure
             {
                 case ProjectorPacket.PayloadOneofCase.Message:
                     {
+                        if(packet == null || packet.Message == null) break;
                         Action action = () => OnStringMessageReceived(packet.Message);
                         mainThreadDispatcher.Enqueue(action);
                         break;
                     }
                 case ProjectorPacket.PayloadOneofCase.Point:
                     {
+                        if(packet == null || packet.Point == null) break;
                         Action action = () => OnCollisionReceived(packet.Point);
                         mainThreadDispatcher.Enqueue(action);
                         break;
