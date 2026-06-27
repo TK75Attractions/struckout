6e1cb2efa12bddebcc6f84e60f85e65910d329d3
taichi765 <taichi0209.youtub@gmail.com>
2026-06-27 20:51:40 +0900

    refactor(camera): Place related classes in a same file

--- struckoutCameraApp/app/src/main/java/com/taichi765/struckoutCameraApp/network/TcpSession.kt
+++ struckoutCameraApp/app/src/main/java/com/taichi765/struckoutCameraApp/network/TcpSession.kt
@@ -1,5 +1,6 @@
 package com.taichi765.struckoutCameraApp.network
 
+import kotlinx.coroutines.flow.StateFlow
 import java.io.Closeable
 
 
@@ -9,5 +10,18 @@ interface TcpSession : SessionStateProvider, Closeable {
     interface Factory {
         fun create(): TcpSession
     }
+}
+
+/**
+ * Provides information about session state (e.g. camera id given by TCP server).
+ */
+interface SessionStateProvider {
+    val state: StateFlow<SessionState>
+}
+
+sealed interface SessionState {
+    data class Connected(val cameraID: UInt) : SessionState
+
+    object DisConnected : SessionState
 }
 
