b494e437750872a75eedf87cb427f1cbf97353b7
taichi765 <taichi0209.youtub@gmail.com>
2026-06-27 20:51:40 +0900

    refactor(camera): Place related classes in a same file

--- struckoutCameraApp/app/src/main/java/com/taichi765/struckoutCameraApp/config/ConfigUiState.kt
+++ struckoutCameraApp/app/src/main/java/com/taichi765/struckoutCameraApp/config/ConfigUiState.kt
@@ -10,3 +10,19 @@ data class ConfigUiState(
     val cameraLocation: Struckout.CameraLocation? = null,
     val warningState: WarningState = WarningState()
 )
+
+/**
+ * `true`のとき警告を表示する
+ */
+data class WarningState(
+    val showX: Boolean = false,
+    val showY: Boolean = false,
+    val showZ: Boolean = false
+)
+
+fun WarningState.isAllOk(): Boolean {
+    if (showX) return false
+    if (showY) return false
+    if (showZ) return false
+    return true
+}
--- struckoutCameraApp/app/src/main/java/com/taichi765/struckoutCameraApp/network/types/SessionState.kt
+++
@@ -1,7 +0,0 @@
-package com.taichi765.struckoutCameraApp.network.types
-
-sealed interface SessionState {
-    data class Connected(val cameraID: UInt) : SessionState
-
-    object DisConnected : SessionState
-}
