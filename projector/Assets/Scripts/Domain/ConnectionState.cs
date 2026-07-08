c5663882e600396c3eedf8486c43de6a0886c70b
ME <75.oda.yuto@tsukukoma-gafe.org>
2026-07-08 15:46:59 +0900

    feat: Add some enums

---
+++ projector/Assets/Scripts/Domain/ConnectionState.cs
@@ -0,0 +1,11 @@
+namespace Struckout.Domain
+{
+    public enum ConnectionState
+    {
+        Disconnected,
+        Connected,
+        Connecting,
+        Disconnecting,
+        Failed
+    }
+}
---
+++ projector/Assets/Scripts/Domain/NetworkConnectionResult.cs
@@ -0,0 +1,10 @@
+namespace Struckout.Domain
+{
+    public enum NetworkConnectionResult
+    {
+        Success,
+        ClientConnectFailed,
+        MasterConnectFailed,
+        InvalidConfiguration
+    }
+}
