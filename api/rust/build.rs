32479d1e340e05666de36508577b847a064de6ea
taichi765 <taichi0209.youtub@gmail.com>
2026-07-09 15:02:42 +0900

    chore: Rerun build script when proto files change

--- api/rust/build.rs
+++ api/rust/build.rs
@@ -1,11 +1,19 @@
+use std::{path::PathBuf, str::FromStr};
+
 fn main() -> std::io::Result<()> {
-    prost_build::compile_protos(
-        &[
-            "../proto/struckout.proto",
-            "../proto/collision.proto",
-            "../proto/xtask_sync.proto",
-        ],
-        &["../proto/"],
-    )?;
+    let proto_files = [
+        "../proto/struckout.proto",
+        "../proto/collision.proto",
+        "../proto/xtask_sync.proto",
+        "../proto/master_and_projector.proto",
+    ];
+    let includes = ["../proto/"];
+    proto_files
+        .iter()
+        .chain(includes.iter())
+        .map(|s| PathBuf::from_str(s).unwrap())
+        .for_each(|path| println!("cargo::rerun-if-changed={}", path.display()));
+
+    prost_build::compile_protos(&proto_files, &includes)?;
     Ok(())
 }
