01c688530e0ac08b9e6acfd2c07419d294a3cde5
taichi765 <taichi0209.youtub@gmail.com>
2026-07-14 12:27:22 +0900

    feat(master): Add remaining_time management in SessionManager

--- game_master/src/session.rs
+++ game_master/src/session.rs
@@ -1,12 +1,13 @@
 use parking_lot::RwLock;
-use std::{cell::RefCell, fmt::Display, rc::Rc, sync::Arc};
-
-use chrono::Duration;
+use slint::{Timer, TimerMode};
+use std::{cell::RefCell, fmt::Display, rc::Rc, sync::Arc, time::Duration};
+use tokio::sync::mpsc;
 
 use crate::data::projector::ProjectorConnection;
 
 const TIMELIMIT: RemainingTime = RemainingTime { mins: 1, secs: 30 };
 
+#[derive(Debug)]
 pub struct SessionManager(Arc<RwLock<SessionManagerInner>>);
 
 impl SessionManager {
@@ -43,6 +44,39 @@ impl SessionManager {
         Self(inner)
     }
 
+    pub fn start_session(&mut self) {
+        {
+            let mut guard = self.0.write();
+            guard.session = Some(Session::new());
+        }
+        let timer = Timer::default();
+        let (tx, mut rx) = mpsc::channel(1);
+        timer.start(TimerMode::Repeated, Duration::from_secs(1), {
+            let this = Arc::clone(&self.0);
+            let tx = tx.clone();
+            move || {
+                {
+                    let mut guard = this.write();
+                    let session = guard.session.as_mut().expect("session should be started");
+                    let is_end = session.remaining_time.count_down();
+                    if is_end {
+                        tx.send(());
+                    }
+                }
+                let guard = this.read();
+                let session = guard.session.as_ref().unwrap(); // checked above
+                if let Some(sub) = &guard.subscriber {
+                    sub.on_remaining_time_changed(&session.remaining_time);
+                }
+            }
+        });
+        slint::spawn_local(async move {
+            rx.recv().await.unwrap();
+            timer.stop();
+        })
+        .unwrap();
+    }
+
     pub fn subscribe(&mut self, sub: impl SessionSubscriber + 'static) {
         self.0.write().subscriber = Some(Box::new(sub));
     }
@@ -66,6 +100,23 @@ impl Display for RemainingTime {
     }
 }
 
+impl RemainingTime {
+    /// Returns true if count became 00:00.
+    fn count_down(&mut self) -> bool {
+        if self.secs != 0 {
+            self.secs -= 1;
+            false
+        } else {
+            if self.mins == 0 {
+                true
+            } else {
+                self.mins -= 1;
+                false
+            }
+        }
+    }
+}
+
 struct SessionManagerInner {
     session: Option<Session>,
     subscriber: Option<Box<dyn SessionSubscriber + 'static>>,
@@ -80,9 +131,35 @@ impl SessionManagerInner {
     }
 }
 
+impl std::fmt::Debug for SessionManagerInner {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("SessionManagerInner")
+            .field("session", &self.session)
+            .field(
+                "subscriber",
+                if self.subscriber.is_some() {
+                    &"Some( .. )"
+                } else {
+                    &"None"
+                },
+            )
+            .finish()
+    }
+}
+
+#[derive(Debug)]
 pub struct Session {
     cur_score: u32,
-    remaining_time: Duration,
+    remaining_time: RemainingTime,
+}
+
+impl Session {
+    fn new() -> Self {
+        Self {
+            cur_score: 0,
+            remaining_time: TIMELIMIT,
+        }
+    }
 }
 
 #[cfg(test)]
