1b33515a3bb4e8ec055c66658d3653027b549185
taichi765 <taichi0209.youtub@gmail.com>
2026-09-05 11:37:46 +0900

    chore: Move game_master/ to touchpanel/

--- game_master/src/main.rs
+++ game_master/src/main.rs
@@ -1,13 +1,3 @@
-use game_master::run_main;
-use tracing::Level;
-use tracing_subscriber::FmtSubscriber;
-
 fn main() {
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::DEBUG)
-        .finish();
-
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-
-    run_main();
+    println!("Hello, world!");
 }
