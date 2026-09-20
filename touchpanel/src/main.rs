337128eb58cf2db7e7340be791cbd60c914a3901
taichi765 <taichi0209.youtub@gmail.com>
2026-09-20 10:43:47 +0900

    refactor(touchpanel): Elaborate tracing filter

--- touchpanel/src/main.rs
+++ touchpanel/src/main.rs
@@ -1,13 +1,20 @@
 use touchpanel::run_main;
 use tracing::Level;
-use tracing_subscriber::FmtSubscriber;
+use tracing_subscriber::{Layer, filter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 fn main() {
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::DEBUG)
-        .finish();
-
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    let layer = fmt::layer().with_filter(filter::filter_fn(|meta| {
+        meta.module_path()
+            .map(|p| {
+                if p.starts_with("touchpanel") {
+                    true
+                } else {
+                    *meta.level() <= Level::DEBUG
+                }
+            })
+            .unwrap_or(*meta.level() >= Level::DEBUG)
+    }));
+    tracing_subscriber::registry().with(layer).init();
 
     run_main();
 }
