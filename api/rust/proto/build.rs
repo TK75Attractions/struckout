c748b27c75283a59b07b1e036cd7898e0ee5fc8d
taichi765 <taichi0209.youtub@gmail.com>
2026-09-22 10:38:38 +0900

    feat(ball-tracker): Add EventLogger::push_pair()

--- api/rust/proto/build.rs
+++ api/rust/proto/build.rs
@@ -8,6 +8,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let include = "../../proto/";
     println!("cargo::rerun-if-changed={}", include);
 
-    tonic_prost_build::configure().compile_protos(&proto_files, &[include])?;
+    const SERDE_ATTR: &str = "#[derive(serde::Serialize, serde::Deserialize)]";
+    let mut config = tonic_prost_build::Config::new();
+    config.type_attribute(".tk75attractions.struckout.v1.DetectionsPacket", SERDE_ATTR);
+    config.type_attribute(".tk75attractions.struckout.v1.Detection", SERDE_ATTR);
+
+    tonic_prost_build::configure().compile_with_config(config, &proto_files, &[include])?;
     Ok(())
 }
